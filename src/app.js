25f5340cbb9036c3e96df66586fa83077fe46fab
afbeltranv <124716013+afbeltranv@users.noreply.github.com>
2023-04-15 02:04:26 +0000

    Código del proyecto terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,54 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const pronouns = ["my", "our", "your", "her", "his", "their"];
+const adjectives = [
+  "attractive",
+  "bald",
+  "beautiful",
+  "chubby",
+  "clean",
+  "dazzling",
+  "drab",
+  "elegant",
+  "fancy",
+  "fit",
+  "flabby",
+  "glamorous",
+  "gorgeous",
+  "handsome",
+  "long",
+  "magnificent",
+  "muscular",
+  "plain",
+  "plump",
+  "quaint",
+  "scruffy",
+  "shapely",
+  "short",
+  "skinny",
+  "stocky",
+  "ugly",
+  "unkempt",
+  "unsightly"
+];
+const noun = [
+  "pencil",
+  "phone",
+  "car",
+  "bike",
+  "toy",
+  "computer",
+  "notebook",
+  "screen"
+];
+let k = 0;
+for (j in noun) {
+  for (i in adjectives) {
+    for (h in pronouns) {
+      k++;
+      console.log(pronouns[h] + adjectives[i] + noun[j] + ".com");
+    }
+  }
+}
+console.log("combinaciones: " + k);
+console.log("nouns: " + noun.length);
+console.log("adjectives: " + adjectives.length);
+console.log("pronouns: " + pronouns.length);
